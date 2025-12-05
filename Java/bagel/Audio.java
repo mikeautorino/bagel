fd2d21e97a524935e2369ee1cd1c2484af70be11
Michael A <mikeautorino@proton.me>
2025-12-05 17:25:54 -0500

    Audio.java rewritten to use javax instead of javafx

--- Java/bagel/Audio.java
+++ Java/bagel/Audio.java
@@ -1,7 +1,7 @@
 package bagel;
 
 import java.io.File;
-
+import javax.sound.sampled.*;
 
 /**
  *  Load an audio file (sound or music). 
@@ -17,94 +17,92 @@ import java.io.File;
  */
 public class Audio
 {
-	AudioClip sound;
-	Media music;
-	MediaPlayer musicPlayer;
+	private Clip clip;
+	private float volume;
 
 	/**
 	 *  Empty constructor for internal methods; 
 	 *  use {@link #loadMusic(String)} or {@link #loadSound(String)} to create an instance.
 	 */
 	Audio()
-    {  }
-    
+	{
+		this.volume = 1.0f;
+	}
+	
 	/**
 	 * Create an audio object from a sound file.
 	 * @param fileName name of the sound file
 	 * @return an Audio object
 	 */
 	public static Audio loadSound(String fileName)
-    {
-        Audio audio = new Audio();
-        audio.sound = new AudioClip( new File(fileName).toURI().toString() );
-        return audio;
-    }
-    
-	/**
-	 * Create an audio object from a music file.
-	 * @param fileName name of the music file
-	 * @return an Audio object
-	 */
-	public static Audio loadMusic(String fileName)
-    {
-        Audio audio = new Audio();
-        audio.music = new Media( new File(fileName).toURI().toString() );
-        audio.musicPlayer = new MediaPlayer( audio.music );
-        return audio;
-    }
-    
+	{
+		Audio audio = new Audio();
+		try
+		{
+			File audioFile = new File(fileName);
+			AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+			audio.clip = AudioSystem.getClip();
+			audio.clip.open(audioStream);
+		}
+		catch (Exception e)
+		{
+			System.err.println("Error loading sound file: " + fileName);
+			e.printStackTrace();
+		}
+		return audio;
+	}
+	
 	/**
 	 * Set whether this audio should repeat forever or play once.
 	 * @param loop whether this audio should repeat forever
 	 */
 	public void setLoop(boolean loop)
-    {
-		int repeatCount = 1;
-		
-		if (loop)
-			repeatCount = Integer.MAX_VALUE;
-		
-        if (sound != null)
-            sound.setCycleCount(repeatCount);
-            
-        if (music != null)
-            musicPlayer.setCycleCount(repeatCount);
-    }
-    
+	{
+		if (clip != null)
+		{
+			if (loop)
+				clip.loop(Clip.LOOP_CONTINUOUSLY);
+			else
+				clip.loop(0);
+		}
+	}
+	
 	/**
 	 * Set volume of audio playback. 
 	 * @param volume value between 0.0 (silent) and 1.0 (original volume).
 	 */
 	public void setVolume(double volume)
-    {
-        if (sound != null)
-            sound.setVolume(volume);
-            
-        if (music != null)
-            musicPlayer.setVolume(volume);
-    }
-    
+	{
+		this.volume = (float) Math.max(0.0, Math.min(1.0, volume));
+		
+		if (clip != null)
+		{
+			FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			float dB = (float) (Math.log(this.volume) / Math.log(10.0) * 20.0);
+			volumeControl.setValue(dB);
+		}
+	}
+	
 	/**
 	 *  Play the loaded audio file.
 	 */
 	public void play()
-    {
-        if (sound != null)
-            sound.play();
-            
-        if (music != null)
-            musicPlayer.play();
-    }
+	{
+		if (clip != null)
+		{
+			clip.setFramePosition(0);
+			clip.start();
+		}
+	}
 	
 	/**
 	 * Stop the audio from currently playing.
 	 */
 	public void stop()
-    {
-        if (sound != null)
-            sound.stop();
-            
-        if (music != null)
-            musicPlayer.stop();
-    }
+	{
+		if (clip != null)
+		{
+			clip.stop();
+		}
+	}
 }
